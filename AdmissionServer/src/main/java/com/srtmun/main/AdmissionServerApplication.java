a3298b9968a4b0e27653b745cb0cba6400b732df
Sonali-m30 <smandalwar30@gmail.com>
2023-05-03 12:20:31 +0530

    first commit

---
+++ AdmissionServer/src/main/java/com/srtmun/main/AdmissionServerApplication.java
@@ -0,0 +1,13 @@
+package com.srtmun.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmissionServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdmissionServerApplication.class, args);
+	}
+
+}
---
+++ AdmissionServer/src/test/java/com/srtmun/main/AdmissionServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srtmun.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmissionServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
